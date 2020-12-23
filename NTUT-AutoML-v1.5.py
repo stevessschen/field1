ce0f347dc711d810e0c77027701a1485d6b679b8
stevessschen <36218346+stevessschen@users.noreply.github.com>
2020-12-23 20:33:54 +0800

    Update NTUT-AutoML-v1.5.py

--- NTUT-AutoML-v1.5.py
+++ NTUT-AutoML-v1.5.py
@@ -2,6 +2,8 @@
 #Version 1.5 Added KMeans and autocluster
 
 import streamlit as st 
+import os #Error: OpenBLAS blas_thread_init: pthread_create: Resource temporarily unavailable
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
 import numpy as np 
 import datetime
 import matplotlib.pyplot as plt
